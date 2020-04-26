f517a60c1f8aaa9c241e6cc357cb85f6f59ab783
crawler-w <2667209799@qq.com>
2020-04-26 21:39:31 +0800

    submit code

---
+++ test_4_26/test_4_26/test.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//#include <assert.h>
+//char* My_strcpy(char* dest, const char* scr)
+//{
+//	assert(dest != NULL);
+//	assert(scr != NULL);
+//	char* ret = dest;
+//	//while (*scr)
+//	//{
+//	//	*dest = *scr;
+//	//	scr++;
+//	//	dest++;
+//	//}
+//	//*dest = *scr;
+//	while (*dest++ = *scr++)
+//		;
+//	return ret;
+//}
+//int main()
+//{
+//	char arr[20] = { 0 };
+//	char* p = "hello bit!";
+//	My_strcpy(arr, p);
+//	printf("%s\n", arr);
+//	return 0;
+//}
