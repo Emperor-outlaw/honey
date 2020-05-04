4acb188b4ecdaca7c5c54f05ef3c990f88114896
crawler-w <2667209799@qq.com>
2020-05-04 20:58:32 +0800

    just view

---
+++ test_4_27/test_4_27/test.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//库函数的实现（自己）
+//strcpy——字符串拷贝
+#include <stdio.h>
+int mian()
+{
+	char arr[20] = { 0 };
+	char *str = "abcdefg";
+	My_strcpy(arr, str);
+	printf()
+	return 0;
+}
