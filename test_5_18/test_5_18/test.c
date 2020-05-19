3b324c17924c391459adb00a1ed831a6d09d8878
crawler-w <2667209799@qq.com>
2020-05-19 15:22:46 +0800

    命令行提交代码

--- test_5_18/test_5_18/test.c
+++ test_5_18/test_5_18/test.c
@@ -101,3 +101,140 @@
 //	return 0;
 //}
 
+////哈希查找法——采用线性探测处理冲突
+//#include <stdio.h>
+//#include <string.h>
+//#include <stdlib.h>
+//#define Tablesize 20
+//typedef enum Status
+//{
+//	fail,
+//	success
+//}Status;
+//typedef struct HashTable
+//{
+//	int* date;
+//	int count;//存放的元素个数
+//}HashTable;
+//int Hash(key)//哈希函数
+//{
+//	return key % 13;
+//}
+//void Init_Hash(HashTable* PH)
+//{
+//	PH->date = (int*)calloc(Tablesize, sizeof(int));
+//}
+//Status Create_Hash(HashTable* PH,int arr[],int sz)
+//{
+//	Init_Hash(PH);
+//	Status s = fail;
+//	int p = 0;
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		while (1)
+//		{
+//			p = Hash(arr[i]);
+//begin:  	    	if (PH->date[p] == 0)
+//			{
+//				PH->date[p] = arr[i];
+//				PH->count++;
+//				break;
+//			}
+//			else
+//			{
+//				p = Hash(p + 1);//处理冲突
+//				goto begin;
+//			}
+//		}
+//	}
+//	if (PH->count == sz)
+//		s = success;
+//	return s;
+//}
+//Status Search_Hash(HashTable H, int key, int* p, int* c)
+//{
+//	Status s = fail;
+//	*p = Hash(key);
+//	while (H.date[*p])
+//	{
+//		if (H.date[*p] != key)
+//		{
+//			*p = Hash(*p + 1);//查找下一处
+//			(*c)++;
+//		}
+//		else
+//		{
+//			s = success;
+//			(*c)++;
+//			return s;
+//		}
+//	}
+//	return s;
+//}
+//void Recreat_Hash(HashTable* PH)
+//{
+//	int* ptr = NULL;
+//	ptr = realloc(PH->date, (Tablesize + Tablesize / 4)* sizeof(int));//每次增加原来表长的1/4
+//	if (ptr)
+//		return;
+//	else
+//		PH->date = ptr;
+//}
+//Status Insert_Hash(HashTable* PH, int e)
+//{
+//	Status s = fail;
+//	int p = 0;//哈希地址
+//	int c = 0;//冲突的次数
+//	if (Search_Hash(*PH, e, &p, &c))
+//		return s;//数据已在哈希表中不需要插入
+//	if (c < Tablesize / 2)
+//	{
+//begin:		p = Hash(e);
+//		while (1)
+//		{
+//			if (PH->date[p])
+//				p = Hash(p + 1);//处理冲突
+//			else
+//			{
+//				PH->date[p] = e;
+//				PH->count++;
+//				s = success;
+//				return s;
+//			}
+//		}
+//	}
+//	else
+//	{
+//		Recreat_Hash(PH);//重建哈希表(不需要对原来的数据进行拷贝，因为recalloc只在在原来空间后面增加了空间)
+//		printf("重建哈希表成功！\n");
+//		goto begin;
+//	}
+//}
+//void Print(HashTable* PH)
+//{
+//	int i = 0; 
+//	for (i = 0; i < Tablesize; i++)
+//	{
+//		printf("%d ", PH->date[i]);
+//	}
+//}
+//int main()
+//{
+//	//关键字序列
+//	int arr[] = { 19, 14, 23, 01, 68, 20, 84, 27, 55, 11, 10, 79 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int p = 0;//哈希地址
+//	int c = 0;//冲突次数
+//	int key = 01;//要查找的关键字
+//	int e = 15;//要插入的元素
+//	HashTable H ;//定义一个哈希表
+//	Create_Hash(&H, arr, sz);//创建哈希表
+//	Insert_Hash(&H, e);
+//	Search_Hash(H, key, &p, &c);
+//	Print(&H);
+//	free(H.date);
+//	H.date = NULL;
+//	return 0;
+//}
+
