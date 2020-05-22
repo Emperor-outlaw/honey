e9257c50edebd3174cf81e13fbc98635fa9b10c5
crawler-w <2667209799@qq.com>
2020-05-22 22:26:31 +0800

    命令行提交代码

--- test_5_21/test_5_21/test.c
+++ test_5_21/test_5_21/test.c
@@ -77,3 +77,180 @@
 //	printf("%d\n", sum);
 //	return 0;
 //}
+
+////打印
+////      * 
+////     *** 
+////    ***** 
+////   ******* 
+////  ********* 
+//// *********** 
+////*************
+//// ***********
+////  *********
+////   ******* 
+////    ***** 
+////     *** 
+////      * 
+//#include <stdio.h>
+//int main()
+//{
+//	int line = 0;
+//	printf("请输入行数;>");
+//	scanf("%d", &line);
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < line; i++)
+//	{
+//		for (j = 0; j < line - i - 1; j++)
+//		{
+//			printf(" ");//先打印空格
+//		}
+//		for (j = 0; j < i * 2 + 1; j++)
+//		{
+//			printf("*");//再打印星号
+//		}
+//		printf("\n");
+//	}
+//	for (i = 0; i < line - 1; i++)
+//	{
+//		for (j = 0; j < i + 1; j++)
+//		{
+//			printf(" ");//先打印空格
+//		}
+//		for (j = 0; j < (line - i - 1) * 2 - 1; j++)
+//		{
+//			printf("*");//再打印星号
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+////编写一个程序，求任意数区间的水仙花数并输出（通式）
+//#include <stdio.h>
+//#include <math.h>
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i < 100000; i++)
+//	{
+//		int count = 0;
+//		int temp = i;
+//		while (temp)
+//		{
+//			temp /= 10;
+//			count++;
+//		}
+//		temp = i;
+//		double sum = 0;
+//		while (temp)
+//		{
+//			sum += pow(temp % 10, count);
+//			temp /= 10;
+//		}
+//		if (sum == i)
+//			printf("%d ", i);
+//	}
+//	return 0;
+//}
+
+////Sn = a + aa + aaa + aaaa + aaaaa  求n项这样的和，其中a是数字
+////例如：2+22+222+2222+22222 —— 5项2的和 
+//#include <stdio.h>
+//#include <math.h>
+//int main()
+//{
+//	//第一种
+//	//int num = 0;
+//	//int sum = 0;
+//	//int i = 0;
+//	//int n = 0;
+//	//printf("请输入你要求哪个数字的几项和:>");
+//	//scanf("%d %d", &num, &n);
+//	//for (i = 1; i <= n; i++)
+//	//{
+//	//	int temp = 0;
+//	//	int tmp = i;
+//	//	while (tmp--)
+//	//	{
+//	//		temp += num*pow(10, tmp);//计算每个加项（每次求一个加项）
+//	//	}
+//	//	sum += temp;//对每个加项求和
+//	//}
+//	//printf("%d\n", sum);
+//
+//	//第二种
+//	int num = 0;
+//	int n = 0;
+//	int sum = 0;
+//	printf("请输入你要求哪个数字的几项和:>");
+//	scanf("%d %d", &num, &n);
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		j = j * 10 + num;
+//		sum += j;
+//	}
+//	printf("%d \n", sum);	
+//	return 0;
+//}
+
+////编写一个程序，可以一直接收键盘字符，
+////如果接收是小写字符就输出大写字符；如果接收是大写字符就输出小写字符 
+////如果是数字字符就不输出 
+//#include <stdio.h>
+//#include <ctype.h>
+//#include <stdlib.h>
+//int main()
+//{
+//	//自己写判断条件
+//	/*char ch = 0;
+//	do
+//	{
+//	    ch = getchar();
+//		if (ch >= 'a' && ch =< 'z')
+//			putchar(ch - 32);
+//		else if (ch >= 'A' && ch =< 'Z')
+//			putchar(ch + 32);
+//		else if (ch >= '0' && ch =< '9')
+//			;        //continue
+//		else
+//			putchar(ch);
+//	} while (ch != EOF);*/
+//
+//	//利用库函数当判断条件
+//	char ch = 0;
+//	while (ch != EOF)
+//	{
+//		ch = getchar();
+//		if (islower(ch))
+//			putchar(toupper(ch));
+//		else if (isupper(ch))
+//			putchar(tolower(ch));
+//		else if (isdigit(ch))
+//			;
+//		else
+//			putchar(ch);
+//	}
+//	return 0;
+//}
+
+
+////函数的递归调用：把1234循序打印在屏幕上1 2 3 4
+//#include <stdio.h>
+//void print(int n)
+//{
+//	if (n > 0)
+//	{
+//		print(n / 10);
+//		printf("%d ", n % 10);
+//	}
+//}
+//int main()
+//{
+//	int n = 1234;
+//	print(n);
+//	return 0;
+//}
