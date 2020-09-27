3ab2ac221f227e4bb91fea4e9bf5533220954545
crawler-w <2667209799@qq.com>
2020-09-27 20:39:31 +0800

    命令行提交代码

---
+++ C++书写HelloWorld/C++书写HelloWorld/test_7_30.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+int main()
+{
+	//cout << "hello world !" << endl;
+	//system("pause");
+	//return 0;
+
+
+	int day = 7;
+	cout << "一周有：" << day << "天" << endl;
+	int month = 12;
+	cout << "一年有：" << month << "月" << endl;
+	return 0;
+}
