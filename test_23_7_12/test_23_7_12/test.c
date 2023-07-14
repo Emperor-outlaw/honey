a66b4e3a18eb9746d64e80e68dae045d366f9aa5
crawler-w <2667209799@qq.com>
2023-07-14 16:27:37 +0800

    交换数据

---
+++ test_23_7_12/test_23_7_12/test.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+
+
+////交换任何数据类型的数据
+//void swap(void* p1, void* p2, size_t num)
+//{
+//	size_t i = 0;
+//	for (i = 0; i < num; i++)
+//	{
+//		char tmp = *((char*)p1 + i);
+//		*((char*)p1 + i) = *((char*)p2 + i);
+//		*((char*)p2 + i) = tmp;
+//	}
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	swap(&a, &b, sizeof(a)); //交换两个整形
+//	printf("a = %d, b = %d\n", a, b);
+//
+//	char c = 'a';
+//	char d = 'b';
+//	swap(&c, &d, sizeof(c)); //交换两个整形
+//	printf("c = %c, d = %c\n", c, d);
+//	return 0;
+//}
+
+
+int main()
+{
+	if(1)
+	{
+		printf("malloc");
+		return 1;
+	}
+	return 0
+}
